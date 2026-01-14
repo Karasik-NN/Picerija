535699236b58ee181c137257bcb9e2931e7b4dde
Karasik-NN <nikita.nikodimovs@sk.lvt.lv>
2026-01-14 10:10:37 +0200

    klase Order ir pabeigts

--- src/picerija/Order.java
+++ src/picerija/Order.java
@@ -1,5 +1,22 @@
 package picerija;
 
 public class Order {
+private String customerName;
+private String phoneNumber;
+private String address;
+private Picca pizza;
+private boolean isDelivery;
+private double totalPrice;
 
+
+public Order(String customerName, String phoneNumber,
+		String address, Picca pizza, boolean isDelivery) {
+	this.customerName = customerName;
+	this.phoneNumber = phoneNumber;
+	this.address = address;
+	this.pizza = pizza;
+	this.isDelivery = isDelivery;
+	this.totalPrice = calcFinPrice();
+	
+}
 }
