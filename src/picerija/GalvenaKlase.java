0b3a9d255f17734eada8577a03142d3e0d3f4ef6
Karasik-NN <nikita.nikodimovs@sk.lvt.lv>
2026-01-14 10:37:59 +0200

    Saku GUI izveidošanu GalvenaKlase

--- src/picerija/GalvenaKlase.java
+++ src/picerija/GalvenaKlase.java
@@ -1,10 +1,14 @@
 package picerija;
 
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
 public class GalvenaKlase {
 
 	public static void main(String[] args) {
+		private JTextField nameField, phoneField, addressField;
 		
-
+private JComboBox<String> typeBox, sizeBox;
 	}
 
 }
