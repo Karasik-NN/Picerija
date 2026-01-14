4600cea486cb0ae4781e64b29898151ec927e436
Karasik-NN <nikita.nikodimovs@sk.lvt.lv>
2026-01-14 10:01:23 +0200

    pabeidzu klase Picca

--- src/picerija/Picca.java
+++ src/picerija/Picca.java
@@ -1,7 +1,62 @@
 package picerija;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class Picca {
-
 	
-
+	 private String name;
+	 private String size;
+	 private ArrayList<String> toppings;
+	 private String sauce;
+	 private double basePrice;
+	 private double price;
+	 
+	 public Picca(String name, String size, String sauce) {
+		 this.name = name;
+		 this.size = size;
+		 this.sauce = sauce;
+		 this.toppings = new ArrayList<>();
+		 this.basePrice = 0.0;
+	 }
+	private double calcBasePrice(String size) {
+		switch(size.toLowerCase())
+		{
+		case"small":
+		case"mazā":
+			
+			return 5.0;
+			
+		case"medium":
+		case"vidēja":
+			
+			return 8.0;
+			
+		case"large":
+		case"lielā":
+			
+			return 12.0;
+			
+			default:
+			
+			return 8.0;
+					
+		}
+	}
+	
+	public void addTopping(String topping) {
+		this.toppings.add(topping);
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSize() {
+		return size;
+	}
+	public ArrayList<String>getToppings(){
+		return toppings;
+	}
+	public double getBasePrice() {
+		return basePrice;
+	}
 }
