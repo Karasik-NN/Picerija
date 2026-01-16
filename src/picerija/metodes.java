84ba102978a3a4472ad846189fc1231c159a6c54
Karasik-NN <nikitatt3914@gmail.com>
2026-01-16 12:52:24 +0200

    tagad var ievadit pirceja vardu telefona numuru izveliet picas veidu izveliet topingu

--- src/picerija/metodes.java
+++ src/picerija/metodes.java
@@ -12,7 +12,38 @@ import javax.swing.JOptionPane;
 
 public class metodes {
 	public static void saktPasutijumu() {
-	      
+		String name = JOptionPane.showInputDialog("Ievadiet pircēja vārdu:");
+        if (name == null) return;
+
+        String phone = JOptionPane.showInputDialog("Ievadiet telefona numuru:");
+        if (phone == null) return;
+    
+        String[] picasVeidi = {"Margarita", "Kapri", "Studentu", "Havajas"};
+        String veids = (String) JOptionPane.showInputDialog(null, "Izvēlieties picu:", "Picas izvēle",
+                JOptionPane.QUESTION_MESSAGE, null, picasVeidi, picasVeidi[0]);
+
+        String[] izmeri = {"Mazā", "Vidēja", "Lielā"};
+        String izmers = (String) JOptionPane.showInputDialog(null, "Izvēlieties izmēru:", "Izmērs",
+                JOptionPane.QUESTION_MESSAGE, null, izmeri, izmeri[0]);
+
+       
+        Picca pica = new Picca(veids, izmers, "Tomātu");
+
+    
+        String[] piedevas = {"Dubultais siers", "Bekons", "Sēnes", "Sīpoli", "Olīvas"};
+        while (true) {
+            int result = JOptionPane.showConfirmDialog(null, "Vai vēlaties pievienot papildus piedevas?", "Piedevas", JOptionPane.YES_NO_OPTION);
+            if (result == JOptionPane.YES_OPTION) {
+                String izveletaPiedeva = (String) JOptionPane.showInputDialog(null, "Izvēlieties piedevu:", "Piedevas",
+                        JOptionPane.QUESTION_MESSAGE, null, piedevas, piedevas[0]);
+                if (izveletaPiedeva != null) {
+                    pica.addTopping(izveletaPiedeva);
+                }
+            } else {
+                break;
+            }
+        }
+
         
 	}
 	public static double aprekinatCenu(Picca pizza, boolean isDelivery) {
