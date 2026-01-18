2d1f4d55f85cb3badb93be21f8f49fff63612391
Karasik-NN <nikitatt3914@gmail.com>
2026-01-18 13:28:18 +0200

    Var iestatīt fonu

--- src/picerija/GalvenaKlase.java
+++ src/picerija/GalvenaKlase.java
@@ -1,8 +1,31 @@
 package picerija;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
+public class GalvenaKlase extends JFrame {
 
-public class GalvenaKlase  {
+    public GalvenaKlase() {
+        setTitle("Picu Pasaule - Galvenā Izvēlne");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
 
-    
+        //fons
+        JLabel background = new JLabel(new ImageIcon("fon.jpg"));//fonas bilde
+        background.setBounds(0, 0, 800, 600);
+        setContentPane(background);
+        background.setLayout(null);
+
+       
+       
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new GalvenaKlase().setVisible(true);
+        });
+    }
 }
